245e7196302492c582b6fcf6aa571b5eef918554
Louis Barboutie <louis.barboutie@gmail.com>
2026-02-15 15:57:31 +0100

    Add inheritance from tkinter menu

--- src/menu.py
+++ src/menu.py
@@ -1,22 +1,22 @@
 import tkinter as tk
 
 
-class Menu:
-    def __init__(self, master: tk.Misc) -> None:
-        self.master = master
+class Menu(tk.Menu):
+    def __init__(self, parent: tk.Misc) -> None:
+        super().__init__(parent)
 
-        self.menu = tk.Menu(self.master)
-
-        file_menu = tk.Menu(self.menu, tearoff=0)
+        file_menu = tk.Menu(self, tearoff=0)
         file_menu.add_command(label="Save log as", command=None)
-        self.menu.add_cascade(label="File", menu=file_menu)
+        self.add_cascade(label="File", menu=file_menu)
 
-        data_menu = tk.Menu(self.menu, tearoff=0)
+        data_menu = tk.Menu(self, tearoff=0)
         data_menu.add_command(label="Start acquisition", command=None)
         data_menu.add_command(label="Stop acquisition", command=None)
-        self.menu.add_cascade(label="Data", menu=data_menu)
+        self.add_cascade(label="Data", menu=data_menu)
 
-        network_menu = tk.Menu(self.menu, tearoff=0)
+        network_menu = tk.Menu(self, tearoff=0)
         network_menu.add_command(label="Start server", command=None)
         network_menu.add_command(label="Stop server", command=None)
         network_menu.add_command(label="Select encoding", command=None)
+        network_menu.add_command(label="Select port", command=None)
+        self.add_cascade(label="Network", menu=network_menu)
